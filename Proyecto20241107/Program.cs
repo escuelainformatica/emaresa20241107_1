19aa7cef0ff7589ebfa24a023b32921b8831c1f9
jorgecc <jcastro@eftec.cl>
2024-11-07 15:03:20 -0300

    cuarto cambio

--- Proyecto20241107/Program.cs
+++ Proyecto20241107/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Proyecto20241107.modelos;
+using Proyecto20241107.servicios;
+
+
 Console.WriteLine("Hello, World!");
+
+var envio = new Envio(5, 5, 5, 20, "Avenida Argentina #222, Santiago");
+EnvioSrv.Mostrar(envio);
+Console.WriteLine("El volumen es :" + EnvioSrv.Volumen(envio));
+Console.WriteLine("El precio $:" + EnvioSrv.PrecioEnvio(envio));
